631de0055dbed1016d99b1781df83a1d99254b05
StefanGreve <greve.stefan@outlook.jp>
2024-03-13 23:33:23 +0100

    Implement CSharpSerializer

---
+++ JackTheEnumRipper/Core/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace JackTheEnumRipper.Core
+{
+    public static class Extensions
+    {
+        public static string Repeat(this string source, int times)
+        {
+            return string.Concat(Enumerable.Repeat(source, times));
+        }
+    }
+}
--- JackTheEnumRipper/Serializer/CSharpSerializer.cs
+++ JackTheEnumRipper/Serializer/CSharpSerializer.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
+using JackTheEnumRipper.Core;
 using JackTheEnumRipper.Interfaces;
 using JackTheEnumRipper.Models;
 
@@ -12,7 +16,38 @@ namespace Serializer
 
         public void Serialize(IEnumerable<AbstractEnum> enums, string path)
         {
-            Console.WriteLine(this.Format);
+            StringBuilder builder = new();
+            string tab = " ".Repeat(4);
+
+            foreach (IGrouping<string, AbstractEnum> enumGroup in enums.GroupBy(x => x.Namespace))
+            {
+                builder.AppendLine($"namespace {enumGroup.Key}");
+                builder.AppendLine("{");
+
+                using IEnumerator<AbstractEnum> enumerator = enumGroup.GetEnumerator();
+                bool last = !enumerator.MoveNext();
+
+                while (!last)
+                {
+                    AbstractEnum @enum = enumerator.Current;
+                    builder.AppendLine($"{tab}{@enum.Scope} enum {@enum.Name} : {@enum.Type}");
+                    builder.AppendLine($"{tab}{{");
+
+                    foreach (AbstractField field in @enum.Fields)
+                    {
+                        builder.AppendLine($"{tab}{tab}{field.Name} = {field.Value},");
+                    }
+
+                    builder.AppendLine($"{tab}}}");
+                    last = !enumerator.MoveNext();
+                    if (!last) builder.AppendLine();
+                }
+
+                builder.AppendLine("}");
+            }
+
+            string code = builder.ToString();
+            File.WriteAllText(path, code, encoding: Encoding.UTF8);
         }
     }
 }
